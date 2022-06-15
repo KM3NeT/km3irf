c41b163f2f7dd5380538db0b7df4f606e75768a4
Mikhail Smirnov <mik.smirnov@fau.de>
2022-06-15 02:23:15 +0200

    list_data function was added

---
+++ src/km3irf/data/list.py
@@ -0,0 +1,18 @@
+# from importlib_resources import files
+from glob import glob
+from os.path import getsize
+from prettytable import PrettyTable
+from importlib_resources import files
+
+tab = PrettyTable(["File Path","Size, KB"], align="l")
+
+
+def list_data(tab=tab):
+    for file in glob(f"{files('km3irf')}/data/*.fits", recursive=True):
+        #add row with file name and size in KB
+        tab.add_row([file, round(getsize(filename=file)/float(1<<10), 2)])
+
+    print(tab)
+
+#list_data()
+
