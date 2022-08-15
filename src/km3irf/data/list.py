aaab8852c7185de3060f08b30205ae60c48898a8
Mikhail Smirnov <mik.smirnov@fau.de>
2022-08-15 16:22:13 +0200

    some changes

--- src/km3irf/data/list.py
+++ src/km3irf/data/list.py
@@ -3,10 +3,11 @@ from os.path import getsize
 from prettytable import PrettyTable
 from importlib_resources import files
 
-tab = PrettyTable(["File Path","Size, KB"], align="l")
+#tab = PrettyTable(["File Path","Size, KB"], align="l")
 
 
-def list_data(tab=tab):
+def list_data():
+    tab = PrettyTable(["File Path","Size, KB"], align="l")
     for file in glob(f"{files('km3irf')}/data/*.fits", recursive=True):
         #add row with file name and size in KB
         tab.add_row([file, round(getsize(filename=file)/float(1<<10), 2)])
