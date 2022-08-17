6faa7352fcebab64cce23b5ad91c09401aeaa438
Mikhail Smirnov <mik.smirnov@fau.de>
2022-08-17 02:17:49 +0200

    some changes

--- src/km3irf/utils.py
+++ src/km3irf/utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from calc import Calculator
+from .calc import Calculator
 from astropy.io import fits
 from os import path
 data_dir = path.join(path.dirname(__file__), 'data')
@@ -58,18 +58,9 @@ def merge_fits(aeff_fits=path.join(data_dir, "aeff.fits"),
     file_bkg = fits.open(bkg_fits)
     hdu_list.append(file_bkg[1])
     hdu_list[4].name = 'BACKGROUND'
-
     
     new_fits_file = fits.HDUList(hdu_list)
-    # new_fits_file.writeto(f'.data/{output_file}', overwrite=True)
-    # new_fits_file.writeto(output_file, overwrite=True)
     new_fits_file.writeto(path.join(data_dir, output_file), overwrite=True)
-    # new_fits_file.writeto(f'.data/{output_file}', overwrite=True)
-    # new_fits_file.close()
-
-    # with fits.open(path.join(data_dir, output_file), mode='update') as new_fits_file:
-    #     new_fits_file = fits.HDUList(hdu_list)
-    #     new_fits_file.writeto(path.join(data_dir, output_file), overwrite=True)
 
     file_aeff.close()
     file_psf.close()
